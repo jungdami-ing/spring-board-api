cd1cddaab9af28c40df58a4b7956bb134014c69d
jungdami <jungdamiing@gmail.com>
2022-05-16 11:59:20 +0900

    spring-board-api commit

---
+++ src/main/java/com/jungdami/springboardapi/SpringBoardApiApplication.java
@@ -0,0 +1,13 @@
+package com.jungdami.springboardapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoardApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoardApiApplication.class, args);
+    }
+
+}
